08ce13c120855ea3e449937a5a3f36d22f0be910
Luiz <luizgustavofcsg@hotmail.com>
2020-09-29 21:26:56 -0300

    JPA Annotations

--- src/main/java/com/example/atendimento/model/Cliente.java
+++ src/main/java/com/example/atendimento/model/Cliente.java
@@ -1,9 +1,22 @@
 package com.example.atendimento.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String nome;
     private String email;
 }
