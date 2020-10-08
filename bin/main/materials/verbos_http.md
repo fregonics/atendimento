386f1e4e831bb5e2d2e67089b27e58923967f385
Luiz <luizgustavofcsg@hotmail.com>
2020-10-08 04:16:27 -0300

    Teste de requests basicos

---
+++ bin/main/materials/verbos_http.md
@@ -0,0 +1,5 @@
+GET     {{host}/clientes}     -> Consultar lista dos clientes
+GET     {{host}/clientes/#ID} -> Consultar cliente com id = #ID 
+POST    {{host}/clientes}     -> Incluir um novo cliente
+PUT     {{host}/clientes/#ID} -> Atualizar dados do cliente
+DELETE  {{host}/clientes/#ID} -> Remover um cliente  
--- src/main/java/com/example/atendimento/atendimento/HelloWorld.java
+++ src/main/java/com/example/atendimento/atendimento/HelloWorld.java
@@ -1,13 +1,35 @@
 package com.example.atendimento.atendimento;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+import com.example.atendimento.model.Cliente;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
     
-    @RequestMapping("/")
-    public String index() {
-        return "hello world";
+    @GetMapping(value = "clientes")
+    public List<Cliente> consultarCliente(@RequestParam String sort) {
+        return List.of(
+            new Cliente(1L, "Luiz", "luizgustavofcsg@hotmail.com"), 
+            new Cliente(2L, sort, "luizgustavofcsg@gmail.com")
+        );
+    }
+
+    @GetMapping("clientes/{id}")
+    public Cliente clienteByID(@PathVariable long id) {
+        return new Cliente(id, "luiz", "luis@asdfasd.com");
+    }
+
+    @PostMapping(value = "clientes")
+    public Cliente incluirCliente(@RequestBody Cliente body) {
+        body.setId(1L);
+        return body;
     }
 }
